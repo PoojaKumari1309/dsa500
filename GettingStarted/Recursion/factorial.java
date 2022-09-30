3005fc8fbbb44f7357d2d0708fc6049c85dff696
PoojaKumari1309 <102920413+PoojaKumari1309@users.noreply.github.com>
2022-09-30 23:00:23 +0530

    Create factorial.java

---
+++ GettingStarted/Recursion/factorial.java
@@ -0,0 +1,14 @@
+
+class Solution{
+    static long factorial(int N){
+        long p=1;
+        for(int i=N;i>0;i--)
+        {
+        p=p*i;
+        
+            
+        }
+        return p;
+        
+    }
+}
