be188b92230b7591a3c83bbf1426e9802ead0fed
PoojaKumari1309 <102920413+PoojaKumari1309@users.noreply.github.com>
2022-09-30 22:50:35 +0530

    Create increasingandDecreasing.java

---
+++ GettingStarted/Recursion/increasingandDecreasing.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner kb=new Scanner(System.in);
+        int n=kb.nextInt();
+        pdi(n);
+    }
+
+    public static void pdi(int n){
+        if(n==0)return;
+        System.out.println(n);
+        pdi(n-1);
+        System.out.println(n);
+        
+        
+    }
+
+}
