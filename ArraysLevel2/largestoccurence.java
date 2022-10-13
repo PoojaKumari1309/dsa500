907b506551f4835a056b3ffc0c8cef3c650437e1
PoojaKumari1309 <102920413+PoojaKumari1309@users.noreply.github.com>
2022-10-14 00:05:32 +0530

    Create largestoccurence.java

---
+++ ArraysLevel2/largestoccurence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority=nums[0];
+        int lead=0;
+        for(int val:nums)
+        {
+            if(val==majority)
+            {
+                lead++;
+            }
+             else if(lead==0)
+            {
+                majority=val;
+                lead=1;
+            }
+            else
+            {
+                lead--;
+            }
+           
+        }
+        return majority;
+        
+    }
+}
