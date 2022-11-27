e3f3777be59c33a000c4d3d5a273bac4114648f1
PoojaKumari1309 <102920413+PoojaKumari1309@users.noreply.github.com>
2022-11-27 22:46:38 +0530

    Create 8.KthFactor.java

---
+++ NumberSystem/8.KthFactor.java
@@ -0,0 +1,18 @@
+public int kthFactor(int n, int k) {
+        List<Integer> res=new ArrayList<>();
+        List<Integer> right=new ArrayList<>();
+        for(int i=1;(i*i)<=n;i++)
+        {
+            if(n%i==0) {
+            res.add(i); 
+               if(i!=n/i)
+               right.add(n/i);
+           }
+        }
+        Collections.reverse(right);
+        res.addAll(right);
+
+        if(k>res.size())return -1;
+        return res.get(k-1);
+        
+    }
