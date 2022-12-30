c3b5d256f77e8f01eed281d94a3daedeb36570d5
PoojaKumari1309 <102920413+PoojaKumari1309@users.noreply.github.com>
2022-12-30 20:16:13 +0530

    Create 4.)ImplementStackUsingQueue.java

---
+++ Queue/4.)ImplementStackUsingQueue.java
@@ -0,0 +1,31 @@
+class MyStack {
+
+    Queue<Integer>q=new ArrayDeque<>();
+    
+    public void push(int x) {
+        q.add(x);
+    }
+    
+    public int pop() {
+        for(int i=1;i<q.size();i++)
+        {
+            q.add(q.remove());
+        }
+        return q.remove();
+    }
+    
+    public int top() {
+       for(int i=1;i<q.size();i++)
+       {
+           q.add(q.remove());
+       } 
+       int ans=q.remove();
+       q.add(ans);
+       return ans;
+        
+    }
+    
+    public boolean empty() {
+        return q.size()==0;
+    }
+}
